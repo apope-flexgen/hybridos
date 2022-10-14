02334bfceea4741bba21e9e715492bef70e8791a
akmuluku <amuluku@flexgen.com>
2022-10-14 12:44:51 -0400

    adding components to docker.sh

--- docker.sh
+++ docker.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
+cwd=$(pwd)
+
 shortTag=`echo $dockerTag | sed 's/v//g'`
 
 products=("ess_controller_meta" "site_controller_meta" "fleet_manager_meta" "twins_meta")
 for productName in "${products[@]}"; do
 	dockerName=`echo $productName | sed 's/_meta//g'`
 	docker build . -t flexgen/"${dockerName}":"${shortTag}" --build-arg productName="${productName}" --build-arg dockerName="${dockerName}" --build-arg verNum="${shortTag}"
-	docker tag flexgen/"${dockerName}":"${shortTag}" flexgen/"${productName}":latest
+	docker tag flexgen/"${dockerName}":"${shortTag}"
 	docker push flexgen/"${dockerName}":"${shortTag}"
-	docker push flexgen/"${dockerName}":latest
 done
 
 components=("cloud_sync" "dts")
+for componentName in "${components[@]}"; do
+	if cd "$i" ; then
+		docker build . -t flexgen/"${componentName}":"${shortTag}" --build-arg verNum="${shortTag}"
+		docker tag flexgen/"${componentName}":"${shortTag}"
+		docker push flexgen/"${componentName}":"${shortTag}"
+		cd $cwd
+	else
+		echo -e "failed to find component $componentName, exiting."
+		exit 1
+	fi
+done
