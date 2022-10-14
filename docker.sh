d2f95386481a34bc64670bdaa99dba8c398ffc55
akmuluku <amuluku@flexgen.com>
2022-10-14 13:16:59 -0400

    cleaning up docker.sh, updating cloud_sync/dts to 2.0.0-rc

--- docker.sh
+++ docker.sh
@@ -7,7 +7,6 @@ products=("ess_controller_meta" "site_controller_meta" "fleet_manager_meta" "twi
 for productName in "${products[@]}"; do
 	dockerName=`echo $productName | sed 's/_meta//g'`
 	docker build . -t flexgen/"${dockerName}":"${shortTag}" --build-arg productName="${productName}" --build-arg dockerName="${dockerName}" --build-arg verNum="${shortTag}"
-	docker tag flexgen/"${dockerName}":"${shortTag}"
 	docker push flexgen/"${dockerName}":"${shortTag}"
 done
 
@@ -15,7 +14,6 @@ components=("cloud_sync" "dts")
 for componentName in "${components[@]}"; do
 	if cd "$i" ; then
 		docker build . -t flexgen/"${componentName}":"${shortTag}" --build-arg verNum="${shortTag}"
-		docker tag flexgen/"${componentName}":"${shortTag}"
 		docker push flexgen/"${componentName}":"${shortTag}"
 		cd $cwd
 	else
