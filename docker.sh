cc2c5cd01fc91af8c380a4d0ff1665d9c0205edb
akmuluku <amuluku@flexgen.com>
2022-10-14 13:42:38 -0400

    debugging docker script

--- docker.sh
+++ docker.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-cwd=$(pwd)
+
+set -x
 
 shortTag=`echo $dockerTag | sed 's/v//g'`
 
-products=("ess_controller_meta" "site_controller_meta" "fleet_manager_meta" "twins_meta")
-for productName in "${products[@]}"; do
-	dockerName=`echo $productName | sed 's/_meta//g'`
-	docker build . -t flexgen/"${dockerName}":"${shortTag}" --build-arg productName="${productName}" --build-arg dockerName="${dockerName}" --build-arg verNum="${shortTag}"
-	docker push flexgen/"${dockerName}":"${shortTag}"
-done
-
-echo -e "$cwd"
+#products=("ess_controller_meta" "site_controller_meta" "fleet_manager_meta" "twins_meta")
+#for productName in "${products[@]}"; do
+#	dockerName=`echo $productName | sed 's/_meta//g'`
+#	docker build . -t flexgen/"${dockerName}":"${shortTag}" --build-arg productName="${productName}" --build-arg dockerName="${dockerName}" --build-arg verNum="${shortTag}"
+#	docker push flexgen/"${dockerName}":"${shortTag}"
+#done
 
 components=("cloud_sync" "dts")
 for componentName in "${components[@]}"; do
 	if cd "$i" ; then
 		docker build . -t flexgen/"${componentName}":"${shortTag}" --build-arg verNum="${shortTag}"
 		docker push flexgen/"${componentName}":"${shortTag}"
-		cd $cwd
+		cd ../
 	else
 		echo -e "failed to find component $componentName, exiting."
 		exit 1
