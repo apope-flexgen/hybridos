236c97f8549b98dcab1a73dc62cceab4e7869ef4
akmuluku <amuluku@flexgen.com>
2022-10-14 13:51:10 -0400

    successful test, docker builds working

--- docker.sh
+++ docker.sh
@@ -4,12 +4,12 @@ set -x
 
 shortTag=`echo $dockerTag | sed 's/v//g'`
 
-#products=("ess_controller_meta" "site_controller_meta" "fleet_manager_meta" "twins_meta")
-#for productName in "${products[@]}"; do
-#	dockerName=`echo $productName | sed 's/_meta//g'`
-#	docker build . -t flexgen/"${dockerName}":"${shortTag}" --build-arg productName="${productName}" --build-arg dockerName="${dockerName}" --build-arg verNum="${shortTag}"
-#	docker push flexgen/"${dockerName}":"${shortTag}"
-#done
+products=("ess_controller_meta" "site_controller_meta" "fleet_manager_meta" "twins_meta")
+for productName in "${products[@]}"; do
+	dockerName=`echo $productName | sed 's/_meta//g'`
+	docker build . -t flexgen/"${dockerName}":"${shortTag}" --build-arg productName="${productName}" --build-arg dockerName="${dockerName}" --build-arg verNum="${shortTag}"
+	docker push flexgen/"${dockerName}":"${shortTag}"
+done
 
 components=("cloud_sync" "dts")
 for componentName in "${components[@]}"; do
