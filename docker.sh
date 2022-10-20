0d2dcad8657ca5a98c8ce096ba8291f2c44f6689
akmuluku <amuluku@flexgen.com>
2022-10-20 00:29:47 -0400

    adding support for imageTag and imageName

--- docker.sh
+++ docker.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# environment variables set by jenkins
+# imageName: centos7
+# imageTag: devel
+# repoName: hybridos
+# dockerTag: v10.1.0
+
 set -x
 
 shortTag=`echo $dockerTag | sed 's/v//g'`
@@ -8,20 +14,21 @@ shortTag=`echo $shortTag | sed 's/-/./g'`
 suffix=""
 pattern='^([1-9]|[1-2][0-9]\d*)\.([0-9]|[1-2][0-9]\d*)\.([0-9]|[1-2][0-9]\d*)\.([a-zA-Z0-9]*)'
 if [[ "$shortTag" =~ $pattern && "$shortTag" != *"release" ]]; then
-    suffix="-snapshot"
+    imageTag="devel" # since we are building a snapshot artifact, we need to pull from the devel image
+	suffix="-snapshot"
 fi
 
 products=("ess_controller_meta" "site_controller_meta" "fleet_manager_meta" "twins_meta")
 for productName in "${products[@]}"; do
 	dockerName=`echo $productName | sed 's/_meta//g'`
-	docker build . -t flexgen/"${dockerName}${suffix}":"${shortTag}" --build-arg productName="${productName}" --build-arg dockerName="${dockerName}" --build-arg verNum="${shortTag}"
+	docker build . -t flexgen/"${dockerName}${suffix}":"${shortTag}" --build-arg imageName="${imageName}" --build-arg imageTag="${imageTag}" --build-arg productName="${productName}" --build-arg dockerName="${dockerName}" --build-arg verNum="${shortTag}"
 	docker push flexgen/"${dockerName}${suffix}":"${shortTag}"
 done
 
 components=("cloud_sync" "dts")
 for componentName in "${components[@]}"; do
 	if cd "$componentName" ; then
-		docker build . -t flexgen/"${componentName}${suffix}":"${shortTag}" --build-arg verNum="${shortTag}"
+		docker build . -t flexgen/"${componentName}${suffix}":"${shortTag}" --build-arg imageName="${imageName}" --build-arg imageTag="${imageTag}" --build-arg verNum="${shortTag}"
 		docker push flexgen/"${componentName}${suffix}":"${shortTag}"
 		cd ../
 	else
