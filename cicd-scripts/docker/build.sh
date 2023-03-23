97fdf614cd18c89f3b99d19322ec5765607ca169
Jeff Gardner <jeff@jeffgardner.dev>
2023-03-23 11:05:15 -0400

    Parameterized the build image in flexgen-cicd.yaml. Extracted buildspec bash commands into standalone scripts.

---
+++ cicd-scripts/docker/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo Running Docker script...
+# ./docker.sh
+echo no-op
---
+++ cicd-scripts/test/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo Installing AWS CLI...
+# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+# unzip awscliv2.zip
+# sudo ./aws/install
+echo no-op
+
+echo Installing Docker...
+# sudo yum install -y yum-utils
+# sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+# sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+echo no-op
