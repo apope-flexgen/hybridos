b3254e240c1830968d158b78a09767c106bbc792
mkeyes-flexgen <107417405+mkeyes-flexgen@users.noreply.github.com>
2023-08-16 13:29:21 -0400

    Update fimslistener to reconnect to fims (#118)
    ## [Link to JIRA Ticket](https://flexgen.atlassian.net/browse/USPOD-922)
    
    ## Root Cause
    - `fims` intermittently receives a non "SUCCESS" message when a node
    client subscribes to a URI, even if the subscription succeeds. When this
    occurs, `fims` drops the connection with the node client. This happens
    especially when a node service is restarted and there is a lot of msg
    traffic for a URI the node service subscribes to.
    
    ## Description
    `fims` could close a `fimsInstance` connection at any time, for any
    reason.
     
    The node process using `fimsInstance` will continually attempt to
    send/receive/subscribe with a connection that has been closed.
    Additionally, the node process using `fimsInstance` will never attempt
    to reconnect unless the node process is restarted.
    
    `fimsListener.js` has been modified to check if a fims connection has
    been closed and attempt to reconnect prior retrying a failed command
    called by the node process. A reconnection attempt will **not** be made
    if the node process itself is responsible for closing the connection.
    
    Also, `SIGINT` and `SIGTERM` are now being caught in order to call
    `closeConnection` prior to the node process exit.
    
    ## Additional bug fixes
    - Fix failed subscribe to URIs being added to `subscribeList` (which
    stores presumably successful subscribed to URIs).
    
    ## How Has This Been Tested?
    
    This change affects all node services equally (metrics, web_server,
    events and dbi). A metrics example is below.
    
    ### metrics VM Testing
    - Rebuild fims and metrics and ensure that hybridos is running
    - Use [echo
    ](https://flexgen.atlassian.net/wiki/spaces/DEVOPS/pages/86933912/How+to+use+Echo+3.x)
    to pub data to `/metrics/timestamp` every 1ms
    - Run `watch -n 5 'kill -15 $(pidof 'metrics')'`
    - Observe that process will intermittently have it's connection dropped
    by fims
    ```
    metrics: Did not get success message from server. Got "{"echo_1":11,"echo_2":22,"echo_3":33}" instead
    
    metrics: proc [metrics] fims 0x4189170 Need to connect before calling receive.
    Failed to send message due to: Broken pipe
    "metrics",  Socket 0, thread 12: error sending success reply to subscription request. rc 88
    ```
    Observe that process reconnects
    ```
    metrics attempting fims reconnect
    fims connection opened by metrics
    ```

--- fims/node_fims/fimsListener.js
+++ fims/node_fims/fimsListener.js
@@ -5,96 +5,166 @@
 const fimsNapi = require('/usr/local/lib/fims.node');
 
 const fimsInstance = new fimsNapi.napi_fims();
-// This call to connect is for backward compatibility only, this can
-// be removed in the future when the other node based modules connect
-// with their own process names
-fimsInstance.connect();
+const maxConnectionRetries = 5;
+
 let subscribeList = [];
+let clientProcessName = '';
+let connectionRetries = 0;
+/**
+ * fims could close the fimsInstance connection at any time, for any reason.
+ * The node process using fimsInstance would have no knowledge of the connection being dropped,
+ * and may need to attempt to reconnect before exec of fims send/receive/subscribe,
+ * so we track isConnectionClosedByClientProcess to know if a reconnect attempt is necessary.
+ * If true, we do not attempt reconnection in processFimsCmd.
+ */
+let isConnectionClosedByClientProcess = true;
 
 module.exports = {
     fimsInstance,
 
     connect(pname = 'Undefined node process') {
-        if (fimsInstance.isConnected()) {
-            fimsInstance.close();
+        if (fimsInstance.isConnected(pname)) {
+            module.exports.closeConnection(pname);
         }
-        fimsInstance.connect(pname);
+
+        const connected = fimsInstance.connect(pname);
+        if (connected) {
+            console.log(`fims connection opened by ${pname}.`);
+            clientProcessName = pname;
+            isConnectionClosedByClientProcess = false;
+            connectionRetries = 0;
+        } else {
+            if (connectionRetries < maxConnectionRetries) {
+                connectionRetries += 1;
+                console.log(`${pname} retrying connect to fims: ${connectionRetries}/${maxConnectionRetries}`);
+                module.exports.connect(pname);
+                return;
+            }
+            console.log(`${pname} failed to connect to fims.`);
+        }
     },
 
     receiveWithTimeout(timeout, callback) {
-        const fimsMessage = new fimsNapi.napi_fims_message();
-        fimsInstance.receiveTimeout(fimsMessage, timeout);
-        let body = fimsMessage.getBody();
-        if (fimsMessage.getMethod() !== 0 && fimsMessage.getUri() !== 0) {
-            try {
-                body = JSON.parse(body);
-            } catch (err) {
-                console.log('There was a problem in receiveWithTimeout: ', err);
-            }
+        return processFimsCmd(() => {
+            const fimsMessage = new fimsNapi.napi_fims_message();
+            fimsInstance.receiveTimeout(fimsMessage, timeout);
+            let body = fimsMessage.getBody();
+            if (fimsMessage.getMethod() !== 0 && fimsMessage.getUri() !== 0) {
+                try {
+                    body = JSON.parse(body);
+                } catch (err) {
+                    console.log('There was a problem in receiveWithTimeout: ', err);
+                }
 
-            const msg = {
-                method: fimsMessage.getMethod(),
-                uri: fimsMessage.getUri(),
-                replyto: fimsMessage.getReplyTo(),
-                body,
-                username: fimsMessage.getUsername(),
-            };
-            callback(msg);
-            return true;
-        }
-        callback();
-        return false;
+                const msg = {
+                    method: fimsMessage.getMethod(),
+                    uri: fimsMessage.getUri(),
+                    replyto: fimsMessage.getReplyTo(),
+                    body,
+                    username: fimsMessage.getUsername(),
+                };
+                callback(msg);
+                return true;
+            }
+            callback();
+            return false;
+        });
     },
 
     send(msg) {
-        let { body } = msg;
-        if (!('username' in msg)){
-            msg.username = null;
-        }
-        if (typeof (body) !== 'string') {
+        return processFimsCmd(() => {
+            let { body } = msg;
+            if (!('username' in msg)){
+                msg.username = null;
+            }
+            if (typeof (body) !== 'string') {
+                try {
+                    body = JSON.stringify(body);
+                } catch (err) {
+                    console.log('There was a problem creating the FIMS message to send: ', err);
+                    return false;
+                }
+            }
             try {
-                body = JSON.stringify(body);
+                const result = fimsInstance.send(msg.method, msg.uri, msg.replyto, body, msg.username);
+                return result;
             } catch (err) {
-                console.log('There was a problem creating the FIMS message to send: ', err);
+                console.log('There was a problem sending to FIMS: ', err);
                 return false;
             }
-        }
-        try {
-            const result = fimsInstance.send(msg.method, msg.uri, msg.replyto, body, msg.username);
-            return result;
-        } catch (err) {
-            console.log('There was a problem sending to FIMS: ', err);
-            return false;
-        }
+        });
     },
 
     subscribeToList(uriList) {
-        return fimsInstance.subscribe(...uriList);
+        return processFimsCmd(() => {
+            return fimsInstance.subscribe(...uriList);
+        });
     },
 
     subscribeTo(uri) {
-        if (!subscribeList.includes(uri)) {
-            subscribeList.push(uri);
-            // console.log('subscribeTo()...about to subscribe to uri_array:',subscribeList);
-            return fimsInstance.subscribe(...subscribeList);
-        }
-        console.log('subscribeTo()...already subscribed to uri ', uri, ' in ', subscribeList);
-        return true;
+        return processFimsCmd(() => {
+            if (!subscribeList.includes(uri)) {
+                // console.log('subscribeTo()...about to subscribe to uri_array:',subscribeList);
+                let subscribed = fimsInstance.subscribe(...[...subscribeList, uri]);
+                if (subscribed) {
+                    console.log(`${clientProcessName} subscribe to ${uri} succeeded.`)
+                    subscribeList.push(uri);
+                    return true;
+                } else {
+                    console.log(`${clientProcessName} subscribe for ${uri} failed.`)
+                    return false;
+                }
+            }
+            console.log('subscribeTo()...already subscribed to uri ', uri, ' in ', subscribeList);
+            return true;
+        });
     },
 
     unsubscribeFrom(uri) {
-        subscribeList = subscribeList.filter((x) => (x !== uri));
-        // console.log('unsubscribeFrom()...about to unsubscribe from uri_array:',subscribeList);
-        if (subscribeList.length) return fimsInstance.subscribe(...subscribeList);
+        return processFimsCmd(() => {
+            subscribeList = subscribeList.filter((x) => (x !== uri));
+            // console.log('unsubscribeFrom()...about to unsubscribe from uri_array:',subscribeList);
+            if (subscribeList.length) return fimsInstance.subscribe(...subscribeList);
 
-        console.log("unsubscribeFrom()...can't subscribe to empty list");
-        return false;
+            console.log("unsubscribeFrom()...can't subscribe to empty list");
+            return false;
+        });
     },
 
-    closeConnection() {
-        console.log('closeConnection()...closing fims connnection');
-        const result = fimsInstance.close();
-
-        return result;
+    closeConnection(pname = 'Undefined node process') {
+        console.log(`${pname} closing fims connnection`);
+        fimsInstance.close(pname);
+        isConnectionClosedByClientProcess = !fimsInstance.isConnected(pname);
+        if (isConnectionClosedByClientProcess) {
+            console.log(`fims connection closed by ${pname}.`);
+        } else {
+            console.log(`${pname} fims connection failed to close.`);
+        }
+        return isConnectionClosedByClientProcess;
     },
 };
+
+function processFimsCmd(cmd) {
+    let success;
+    success = cmd()
+    if (!success) {
+        if (connectionRetries < maxConnectionRetries && isConnectionClosedByFims()) {
+            console.log(`processFimsCmd: ${clientProcessName} attempting fims reconnect.`);
+            module.exports.connect(clientProcessName);
+            return cmd();
+        }
+    }
+    return success
+}
+
+function isConnectionClosedByFims() {
+    return !fimsInstance.isConnected(clientProcessName) && !isConnectionClosedByClientProcess;
+}
+
+['SIGINT', 'SIGTERM'].forEach((eventType) => {
+    process.on(eventType, () => {
+        console.log(`${clientProcessName} closing fims connection due to ${eventType}.`);
+        module.exports.closeConnection(clientProcessName);
+        throw Error(`${eventType} detected.`);
+    });
+});
