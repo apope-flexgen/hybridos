993f4b95fa352cfaac8ff8ad4bbca768927e6dbe
Jacky Wong <105668267+jwong-flexgen@users.noreply.github.com>
2023-08-31 10:09:16 -0400

    USPOD-888 dbi.module.ts tests (#93)
    ## [Link to JIRA Ticket](https://flexgen.atlassian.net/browse/USPOD-888)
    
    ## Description
    - Add unit tests for the dbi module, excluding the actual module file.
    
    ## How Has This Been Tested?
    Run `pnpm test:cov` under `web_apps/packages/hybridos/server`.
    
    Note the 100% test coverage for the `dbi` module, located under
    `web_apps/packages/hybridos/server/src/dbi`.
    
    Note: Please merge #83 first, for `package.json` changes.

---
+++ web_apps/packages/hybridos/server/src/dbi/dbi.mocks.ts
@@ -0,0 +1,20 @@
+import { DBI_URIs } from "./dbi.interface";
+import { FimsMsg } from '../fims/responses/fimsMsg.response';
+
+export const DBI_DEST_URI = DBI_URIs.UI_Config_Assets;
+
+export const FIMS_RESPONSE: FimsMsg = {
+    method: 'method',
+    uri: '/fims/uri',
+    replyto: 'replyto',
+    body: {},
+    username: 'username',
+}
+
+export const FIMS_RESPONSE_ERR: FimsMsg = {
+    method: 'method',
+    uri: '/fims/uri',
+    replyto: 'replyto',
+    body: 'not found in collection',
+    username: 'username',
+}
---
+++ web_apps/packages/hybridos/server/src/dbi/dbi.service.spec.ts
@@ -0,0 +1,83 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { FIMS_SERVICE, IFimsService } from '../fims/interfaces/fims.interface';
+import { IDBIService } from './dbi.interface';
+import { DBIService } from './dbi.service';
+import { DBIDocumentNotFoundException } from './exceptions/dbi.exceptions';
+import { DBI_DEST_URI, FIMS_RESPONSE, FIMS_RESPONSE_ERR } from './dbi.mocks';
+
+describe('AssetsService', () => {
+    let dbiService: IDBIService;
+    let fimsService: IFimsService;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                DBIService,
+                {
+                    provide: FIMS_SERVICE,
+                    useValue: {
+                        get: jest.fn(),
+                        send: jest.fn(),
+                    },
+                },
+            ],
+        }).compile();
+
+        dbiService = module.get<DBIService>(DBIService);
+        fimsService = module.get<IFimsService>(FIMS_SERVICE);
+    });
+
+    it('fimsService should be defined', () => {
+        expect(fimsService).toBeDefined();
+    });
+
+    describe('test getFromDBI', () => {
+        it('should use the URI', async () => {
+            const getFromDBIService = jest
+                .spyOn(fimsService, 'get')
+                .mockResolvedValue(FIMS_RESPONSE);
+            await dbiService.getFromDBI(DBI_DEST_URI);
+            expect(getFromDBIService).toHaveBeenCalledWith(`/dbi${DBI_DEST_URI}`);
+        });
+
+        it('should return DBI body', async () => {
+            jest
+                .spyOn(fimsService, 'get')
+                .mockResolvedValue(FIMS_RESPONSE);
+            expect(await dbiService.getFromDBI(DBI_DEST_URI)).toMatchObject(FIMS_RESPONSE.body)
+        });
+
+        it('should throw DBI exception', async () => {
+            jest
+                .spyOn(fimsService, 'get')
+                .mockResolvedValue(FIMS_RESPONSE_ERR);
+            await expect(dbiService.getFromDBI(DBI_DEST_URI)).rejects.toThrowError(
+                new DBIDocumentNotFoundException(FIMS_RESPONSE_ERR.body)
+            );
+        });
+    });
+
+    describe('test postToDBI', () => {
+        it('should use the arguments', async () => {
+            const postToDBIService = jest
+                .spyOn(fimsService, 'send')
+                .mockResolvedValue(FIMS_RESPONSE);
+            await dbiService.postToDBI(DBI_DEST_URI, 'example data');
+            expect(postToDBIService).toHaveBeenCalledWith({
+                method: 'post',
+                uri: `/dbi${DBI_DEST_URI}`,
+                replyto: '',
+                body: 'example data',
+                username: '',
+            })
+        });
+
+        it('should return DBI body', async () => {
+            jest
+                .spyOn(fimsService, 'send')
+                .mockResolvedValue(FIMS_RESPONSE);
+            expect(await dbiService.postToDBI(DBI_DEST_URI, 'example data'))
+                .toEqual(FIMS_RESPONSE.body);
+        });
+    });
+});
