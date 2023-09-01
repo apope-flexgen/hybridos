b24d4854192cc20bddf199353ad74294988cc979
Jacky Wong <105668267+jwong-flexgen@users.noreply.github.com>
2023-09-01 11:08:40 -0400

    USPOD-910 Support for naked body assets (#135)
    ## [Link to JIRA Ticket](https://flexgen.atlassian.net/browse/USPOD-910)
    
    ## Description
    Add support for naked body assets for the site status bar. Naked body
    assets are defined here as data with a type of
    - `number`,
    - `boolean`,
    - or `string`,
    
    as opposed to a clothed asset of the Object type, e.g.
    ```
    {
      "value": 12345,
      [remainder of object]
    }
    ```
    
    ### A limitation
    In clothed assets, the data's unit is provided along with the value
    displayed. Naked assets do not have an accompanying unit field, so the
    value will appear on the UI as-is.
    
    The same applies to the scalar for a number value, so numbers with a
    large number of digits will have them all displayed on the UI. For
    example, if fims returns a value of `123.456789000001`, then the UI will
    render the exact number `123.456789000001` and take up significant
    horizontal space. **Please advise if this should change, perhaps with a
    max-length applied.**
    
    ## How Has This Been Tested?
    - Set up the Site Status Bar per our
    [docs](https://flexgen.atlassian.net/wiki/spaces/AP/pages/218562588/Non-Page+Specific+Functionality+-+Site+Controller).
    - Amend the `site_status.json` with another `dataSources` entry where
    the data is naked. An example (which may or may not work depending on
    the `config` that you have loaded into HybridOS):
    ```
            {
                "uri": "/assets/ess/ess_1",
                "field": "soc",
                "dataType": "number",
                "label": "ESS 1 SOC %"
            }
    ```
    - Ensure the `site_status.json` file is loaded into DBI in the
    `hybridos_run.sh` script.
    ```
    load_dbi('ui_config', file_document_renames={'CA_configs.json': 'assets', 'layout_configs.json': 'layout', 'site_status.json': 'site_status'})
    ```
    - Build and run HybridOS. In the UI, observe that the naked body asset
    point shows up on the Site Status Bar.

--- web_apps/packages/hybridos/server/src/bff/SiteStatusBar/sitestatus.service.ts
+++ web_apps/packages/hybridos/server/src/bff/SiteStatusBar/sitestatus.service.ts
@@ -1,128 +1,142 @@
-import { Inject, Injectable } from '@nestjs/common'
-import { Observable, map, merge } from 'rxjs'
-import { computeClothedValue } from '../../utils/utils'
-import { FimsService } from '../../fims/fims.service'
-import { FIMS_SERVICE } from '../../fims/interfaces/fims.interface'
-import { SiteStatusConfig, SiteStatusDataField, SiteStatusDataFieldConfig, SiteStatusResponse } from './sitestatus.interface'
-import { DBI_URIs, IDBIService } from 'src/dbi/dbi.interface'
-import { DBI_SERVICE } from 'src/dbi/dbi.interface'
-import { AppEnvService } from 'src/environment/appEnv.service'
-import { SiteConfiguration } from 'src/webuiconfig/webUIConfig.interface'
+import { Inject, Injectable } from '@nestjs/common';
+import { Observable, map, merge } from 'rxjs';
+import { computeClothedValue } from '../../utils/utils';
+import { FimsService } from '../../fims/fims.service';
+import { FIMS_SERVICE } from '../../fims/interfaces/fims.interface';
+import {
+  SiteStatusConfig,
+  SiteStatusDataField,
+  SiteStatusDataFieldConfig,
+  SiteStatusResponse,
+} from './sitestatus.interface';
+import { DBI_URIs, IDBIService } from 'src/dbi/dbi.interface';
+import { DBI_SERVICE } from 'src/dbi/dbi.interface';
+import { AppEnvService } from 'src/environment/appEnv.service';
+import { SiteConfiguration } from 'src/webuiconfig/webUIConfig.interface';
 
-const SITE_SUMMARY_URI = '/site/summary'
+const SITE_SUMMARY_URI = '/site/summary';
 
 @Injectable()
 export class SiteStatusService {
-    private siteConfiguration: SiteConfiguration
-    constructor(
-        @Inject(DBI_SERVICE)
-        private readonly dbiService: IDBIService,
-        @Inject(FIMS_SERVICE)
-        private readonly fimsService: FimsService,
-        private readonly appEnvService: AppEnvService
-    ) {
-        this.siteConfiguration = this.appEnvService.getSiteConfiguration()
-    }
-    config: SiteStatusConfig | undefined
+  private siteConfiguration: SiteConfiguration;
+  constructor(
+    @Inject(DBI_SERVICE)
+    private readonly dbiService: IDBIService,
+    @Inject(FIMS_SERVICE)
+    private readonly fimsService: FimsService,
+    private readonly appEnvService: AppEnvService,
+  ) {
+    this.siteConfiguration = this.appEnvService.getSiteConfiguration();
+  }
+  config: SiteStatusConfig | undefined;
 
-    subscribeToSiteStatus = async (): Promise<
-        Observable<SiteStatusResponse>
-    > => {
-        const config = await this.getConfig()
+  subscribeToSiteStatus = async (): Promise<Observable<SiteStatusResponse>> => {
+    const config = await this.getConfig();
 
-        return this.getObservable(config)
-    }
+    return this.getObservable(config);
+  };
 
-    getConfig = async (): Promise<SiteStatusConfig> => {
-        if (this.config) {
-            return this.config
-        }
-
-        let unindexedConfig = await this.dbiService.getFromDBI(DBI_URIs.SITE_STATUS_BAR);
-        let indexedDataSources = unindexedConfig.dataSources.map(
-            (ds: SiteStatusDataFieldConfig, index: number): SiteStatusDataField => {
-                return {
-                    ...ds,
-                    index,
-                };
-            },
-        );
-        unindexedConfig.dataSources = indexedDataSources;
-        this.config = unindexedConfig;
-
-        return this.config
+  getConfig = async (): Promise<SiteStatusConfig> => {
+    if (this.config) {
+      return this.config;
     }
 
-    getObservable = async (
-        config: SiteStatusConfig
-    ): Promise<Observable<SiteStatusResponse>> => {
-        const fimsSubscribe = this.fimsService.subscribe(SITE_SUMMARY_URI)
+    let unindexedConfig = await this.dbiService.getFromDBI(DBI_URIs.SITE_STATUS_BAR);
+    let indexedDataSources = unindexedConfig.dataSources.map(
+      (ds: SiteStatusDataFieldConfig, index: number): SiteStatusDataField => {
+        return {
+          ...ds,
+          index,
+        };
+      },
+    );
+    unindexedConfig.dataSources = indexedDataSources;
+    this.config = unindexedConfig;
 
-        const dataObservables: Observable<SiteStatusResponse>[] =
-            config.dataSources.map(
-                (dataSource): Observable<SiteStatusResponse> => {
-                    return this.fimsService.subscribe(dataSource.uri).pipe(
-                        map((data): SiteStatusResponse => {
-                            const body = data.body
+    return this.config;
+  };
 
-                            if (!body[dataSource.field]) {
-                                return {} as SiteStatusResponse
-                            }
+  getObservable = async (config: SiteStatusConfig): Promise<Observable<SiteStatusResponse>> => {
+    const fimsSubscribe = this.fimsService.subscribe(SITE_SUMMARY_URI);
 
-                            const fieldDataObject = body[dataSource.field]
+    const dataObservables: Observable<SiteStatusResponse>[] = config.dataSources.map(
+      (dataSource): Observable<SiteStatusResponse> => {
+        return this.fimsService.subscribe(dataSource.uri).pipe(
+          map((data): SiteStatusResponse => {
+            const body = data.body;
 
-                            let value: String = fieldDataObject['value']
-                            let unit: String = fieldDataObject['unit']
+            if (!body[dataSource.field]) {
+              return {} as SiteStatusResponse;
+            }
 
-                            if (dataSource.dataType === 'number') {
-                                const { value: newValue, targetUnit } =
-                                    computeClothedValue(
-                                        Number(fieldDataObject['value']),
-                                        Number(fieldDataObject['scaler']),
-                                        fieldDataObject['unit'],
-                                        this.siteConfiguration
-                                    )
+            const fieldData = body[dataSource.field];
+            const fieldDataType = typeof fieldData;
+            const validDataTypes = ['boolean', 'number', 'string'];
 
-                                value = newValue
-                                unit = targetUnit
-                            }
-
-                            return {
-                                data: {
-                                    dataPoints: {
-                                        [`${dataSource.uri}/${dataSource.field}`]:
-                                            {
-                                                label: dataSource.label,
-                                                value,
-                                                unit,
-                                                index: dataSource.index,
-                                            },
-                                    },
-                                },
-                            } as SiteStatusResponse
-                        })
-                    )
-                }
-            )
-
-        // FIXME: validate the data from fims first
-        const siteStateObs: Observable<SiteStatusResponse> = fimsSubscribe.pipe(
-            map((fimsData) => {
-                const newDTO: SiteStatusResponse = {
-                    data: {
-                        activeFaults: fimsData.body['active_faults'],
-                        activeAlarms: fimsData.body['active_alarms'],
-                        siteState: fimsData.body['site_state'],
-                        siteStatusLabel: this.config.siteStatusLabel || '',
+            if (validDataTypes.includes(fieldDataType)) {
+              return {
+                data: {
+                  dataPoints: {
+                    [`${dataSource.uri}/${dataSource.field}`]: {
+                      label: dataSource.label,
+                      value: fieldData,
+                      unit: '',
                     },
-                }
+                  },
+                },
+              } as SiteStatusResponse;
+            }
 
-                return newDTO
-            })
-        )
+            let value: String = fieldData['value'];
+            let unit: String = fieldData['unit'];
 
-        const obs = merge(siteStateObs, ...dataObservables)
+            if (dataSource.dataType === 'number') {
+              const { value: newValue, targetUnit } = computeClothedValue(
+                Number(fieldData['value']),
+                Number(fieldData['scaler']),
+                fieldData['unit'],
+                this.siteConfiguration,
+              );
 
-        return obs
-    }
+              value = newValue;
+              unit = targetUnit;
+            }
+
+            return {
+              data: {
+                dataPoints: {
+                  [`${dataSource.uri}/${dataSource.field}`]: {
+                    label: dataSource.label,
+                    value,
+                    unit,
+                    index: dataSource.index,
+                  },
+                },
+              },
+            } as SiteStatusResponse;
+          }),
+        );
+      },
+    );
+
+    // FIXME: validate the data from fims first
+    const siteStateObs: Observable<SiteStatusResponse> = fimsSubscribe.pipe(
+      map((fimsData) => {
+        const newDTO: SiteStatusResponse = {
+          data: {
+            activeFaults: fimsData.body['active_faults'],
+            activeAlarms: fimsData.body['active_alarms'],
+            siteState: fimsData.body['site_state'],
+            siteStatusLabel: this.config.siteStatusLabel || '',
+          },
+        };
+
+        return newDTO;
+      }),
+    );
+
+    const obs = merge(siteStateObs, ...dataObservables);
+
+    return obs;
+  };
 }
