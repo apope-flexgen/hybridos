ecfa8a0aad46c8d9490999ab91ea3f990dcfc564
BrandonKnoble1 <105602159+BrandonKnoble1@users.noreply.github.com>
2023-09-12 16:08:17 -0400

    removal of fims_relay from meta (#188)

--- build_utils.sh
+++ build_utils.sh
@@ -5,7 +5,7 @@ name=hybridos
 components=(fims fims_relay ess_controller site_controller fleet_manager modbus_interface dnp3_interface washer dbi scheduler cops ftd cloud_sync dts twins echo overwatch events metrics go_metrics web_apps)
 meta=(ess_controller_meta site_controller_meta fleet_manager_meta twins_meta)
 
-ess_controller_meta=(fims fims_relay ess_controller modbus_interface dnp3_interface dbi ftd cloud_sync overwatch events metrics go_metrics web_apps)
-site_controller_meta=(fims fims_relay site_controller modbus_interface dnp3_interface dbi scheduler cops ftd cloud_sync dts overwatch events metrics go_metrics web_apps)
-fleet_manager_meta=(fims fims_relay fleet_manager modbus_interface dnp3_interface washer dbi scheduler cops ftd cloud_sync dts echo overwatch events metrics go_metrics web_apps)
-twins_meta=(fims fims_relay ess_controller modbus_interface dnp3_interface dbi ftd cloud_sync twins echo overwatch go_metrics)
+ess_controller_meta=(fims ess_controller modbus_interface dnp3_interface dbi ftd cloud_sync overwatch events metrics go_metrics web_apps)
+site_controller_meta=(fims site_controller modbus_interface dnp3_interface dbi scheduler cops ftd cloud_sync dts overwatch events metrics go_metrics web_apps)
+fleet_manager_meta=(fims fleet_manager modbus_interface dnp3_interface washer dbi scheduler cops ftd cloud_sync dts echo overwatch events metrics go_metrics web_apps)
+twins_meta=(fims ess_controller modbus_interface dnp3_interface dbi ftd cloud_sync twins echo overwatch go_metrics)
