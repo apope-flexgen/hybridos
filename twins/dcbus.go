848623317ca05c40227b9cb406d806e4666a57d8
gbriggsFG <106970231+gbriggsFG@users.noreply.github.com>
2023-10-17 11:08:18 -0400

    ACO-102-PSM-Updates-for-DC-Augmentation-demo (#264)
    ## [Link to JIRA Ticket](https://flexgen.atlassian.net/browse/ACO-102)
    
    ## Description
    Implement nominal operation of DC Augmentation in PSM for demo.
    ## How Has This Been Tested?
    
    Via fims:
    Test cases for DC Augmentation
    
    -- Nominal cases
    PCS 100kW Discharge, DCDC zero power. Nominal BMSes	RESULT PASS
    	BMS 1 p ~= 100kW
    	BMS 2 p ~= 0kW
    
    PCS 100 kW Charge, DCDC zero power. Nominal BMSes	RESULT PASS
    	BMS 1 p ~= -100kW
    	BMS 2 p ~= 0kW
    
    PCS Zero power, DCDC 100 kW discharge. Nominal BMSes	RESULT PASS
    	BMS 2 p ~= 100kW
    	BMS 2 p ~= -100kW
    
    PCS zero power, DCDC 100 kW charge. Nominal BMSes 	RESULT PASS
    	BMS 2 p ~= -100kW
    	BMS 1 p ~= 100kW
    
    PCS 100kW Discharge, DCDC 100 kW discharge. Nominal BMSes RESULT PASS
    	BMS 2 p = 100kW
    	BMS 1 p = 0kW
    
    PCS 100kW Discharge, DCDC 50 kW discharge. Nominal BMSes RESULT PASS
    	BMS 2 p = 50kW
    	BMS 1 p = 50kW
    
    PCS 100kW Discharge, DCDC 150 kW discharge. Nominal BMSes RESULT PASS
    	BMS 1 p = -50kW
    	BMS 2 p = 150kW
    
    PCS 100kW charge, DCDC 100 kW charge. Nominal BMSes RESULT PASS
    	BMS 2 p = -100kW
    	BMS 1 p = 0kW
    
    PCS 100kW charge, DCDC 50 kW charge. Nominal BMSes RESULT PASS
    	BMS 2 p = -50kW
    	BMS 1 p = -50kW
    
    PCS 100kW charge, DCDC 150 kW charge. Nominal BMSes RESULT PASS
    	BMS 1 p = 50kW
    	BMS 2 p = -150kW
    
    PCS 100kW charge, DCDC 100kW discharge. Nominal BMSes RESULT PASS
    	BMS1 p = -200kW
    	BMS2 p = 100kW
    
    PCS 100kW Discharge, DCDC 100kW Charge. Nominal BMSes RESULT PASS. 
    	BMS1 p = 200kW
    	BMS2 p = -100kW

--- twins/dcbus.go
+++ twins/dcbus.go
@@ -8,9 +8,14 @@ type dcbus struct {
 	ID              string
 	Vdc				float64
 	Pdc				float64
+	P1Conv			float64     //Power draw from parent-side converting assets (e.g. PCS)
+	P2Conv			float64		//Power from child-side converting assets (e.g. DCDC)
+	Pind 			float64     //Power to be made up by independent source child assets (bms, PV, etc)
 	Aliases         []string
 	dcTerminal		terminal
 	Dvolts          droop
+	Plim			float64 	//Mostly a pass through for default configuration of child assets. No fault behavior for this configuration
+	Vnom			float64 	//Same as above
 }
 
 func (d *dcbus) Init() {
@@ -21,13 +26,16 @@ func (d *dcbus) Init() {
 func (d *dcbus) UpdateMode(input terminal) (output terminal) {
 	//processCtrlWordConfig(f, f.CtrlWord1Cfg, f.CtrlWord1)
 	//d.Vdc = input.v //TODO GB this perhaps should be last step arbitrated voltage instead of coming from above. 
+	d.Pind = 0 			//Independent source power should not carry over from previous step. 
+	d.Pdc = 0
 	output.vdc = d.Vdc
 	return output
 }
 
 //Power converting assets (independent sources) send their power commands up at this stage
-//store them and combine with parent power command in next step to determine delta to be made up by dependent power sources (pv)
+//store them and combine with parent power command in next step to determine delta to be made up by dependent power sources (pv, bms, etc)
 //Direction: up (2)
+//NO
 func (d *dcbus) GetLoadLines(input terminal, dt float64) (output terminal) {
 	d.Pdc = -input.p
 	//terminal{f.V, 0, f.P, f.Q, f.S, f.F2, f.Ph, f.Dactive, f.Dreactive, droop{}}
@@ -40,8 +48,9 @@ func (d *dcbus) GetLoadLines(input terminal, dt float64) (output terminal) {
 //Send down arbitrated voltage setpoint between parent and dependent children. 
 //Direction: down (3)
 func (d *dcbus) DistributeVoltage(input terminal) (output terminal) {
-	pDelta := d.Pdc + input.p //Difference between parent power and child independent source power is the delta that independent sources must attempt to make up
-	output.p = pDelta //need to negate the delta here as PV will need to supply the opposite. 
+	//d.Pind = d.Pdc + input.p //Difference between parent power and child independent source power is the delta that independent sources must attempt to make up
+	d.P1Conv = input.p 
+	output.p = d.Pind //need to negate the delta here as PV will need to supply the opposite. 
 	output.vdc = d.Vdc
 	return output
 }
@@ -50,14 +59,15 @@ func (d *dcbus) DistributeVoltage(input terminal) (output terminal) {
 //Direction: up (4)
 func (d *dcbus) CalculateState(input terminal, dt float64) (output terminal) {
 	d.Vdc = input.vdc
-	d.Pdc = input.p //At this point, input power is the sum of DCDC and PV power in this time step. Should equal PCS power, and we will check this next step. 
+	d.P2Conv = input.p 
+	d.Pind = d.P1Conv - d.P2Conv
 	return output
 }
 
 //Direction: down (5)
 func (d *dcbus) DistributeLoad(input terminal) (output terminal) {
-	pDelta := input.p - d.Pdc //Now this is the difference in power between all assets on the bus. Independent sources must make this up. Likely this will be due to losses
-	output.p = -pDelta
+	// pDelta := input.p - d.Pdc //Now this is the difference in power between all assets on the bus. Independent sources must make this up. Likely this will be due to losses
+	output.p = d.Pind
 	output.vdc = d.Vdc
 	return output
 }
