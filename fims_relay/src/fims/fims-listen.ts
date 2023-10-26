579e8360e7f2075f490327853ced85bdd72ce516
Jack Timothy <51306958+JackTimothy@users.noreply.github.com>
2023-10-26 17:12:03 -0400

    Patch FIMS Relay Memory Leak - dev (#286)
    ## [Link to JIRA Ticket](https://flexgen.atlassian.net/browse/OTCO-624)
    
    ## Description
    Patches FIMS Relay memory leak.
    
    ## How Has This Been Tested?
    Ran container and saw no memory leak where there previously was one.
    
    Co-authored-by: mkeyes <mkeyes@flexgen.com>

--- fims_relay/src/fims/fims-listen.ts
+++ fims_relay/src/fims/fims-listen.ts
@@ -2,14 +2,14 @@ const fims = require('./fimsInstance');
 const { parentPort } = require('worker_threads');
 
 function publish_on_receive() {
-  console.log('***** listening');
-  while (true) {
-    fims.receiveWithTimeout(50, (data) => {
-      if (data) {
-        parentPort.postMessage(data);
-      }
-    });
-  }
+  fims.receiveWithTimeout(50, (data) => {
+    if (data) {
+      parentPort.postMessage(data);
+    }
+  });
+  setTimeout(() => {
+    publish_on_receive();
+  }, 2);
 };
 
 var subscribedToAll = fims.subscribeTo('/');
